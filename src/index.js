27c6306f4d869a67049fbfacfa30008fdd25031a
NEEL SHAH <107039743+neelshah683@users.noreply.github.com>
2023-09-13 21:54:37 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> Hello World! </h1>
+    <p>
+      <h1>This is a paragraph</h1>
+    </p>
+  </div>,
+  document.getElementById("root")
+);
